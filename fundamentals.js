5b638e0d7d343e9f0e2af5b7bad8e43668260fe5
Dominik Martynski <20094523@mail.wit.ie>
2022-11-28 18:55:32 +0000

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -14,4 +14,15 @@ const aCar = {
   };
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
-  console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.countycode + '-' + aCar.registration.number)
+  console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.countycode + '-' + aCar.registration.number);
+
+  aCar.mileage = "10000";
+  aCar.colour = {
+    exterior: "red",
+    interior: {
+        texture : "leather",
+        shade : "cream",
+    }
+  }
+
+  console.log("It is a " + aCar.colour.exterior + " car, " + aCar.mileage + " mileage, with " + aCar.colour.interior.texture + " interior.");
