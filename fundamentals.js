c065ba4bbf7b87c79f2a1bfd12f27bcb90a86cc0
Dominik Martynski <20094523@mail.wit.ie>
2023-08-25 14:03:51 +0100

    Finished lab 1.

--- fundamentals.js
+++ fundamentals.js
@@ -1,12 +1,25 @@
 const aCar = {
-    owner : "Joe Bloggs",
-    type : {
-      Make : 'Toyota',
-      Model : 'Corolla',
-      CC: '1.9',
-    },
-    registration : '201WD1058'
+	owner : 'Joe Bloggs',
+  previous_owners: [
+    { name: "Pat Smith", address: "1 Main Street"},
+    { name: "Sheila Dwyer", address: "2 High Street"}
+  ],
+	type : {
+		make : 'Toyota',
+		model : 'Corolla',
+		cc : 1.8
+	},
+  registration: { year: 201, county: "WD", number: 1058 }
 };
+console.log(aCar.owner + " drives a " + aCar.type.make);
+console.log(
+  "Reg. = " +
+    aCar.registration.year +
+    "-" +
+    aCar.registration.county +
+    "-" +
+    aCar.registration.number
+);
 
 aCar.milage = 10000
 aCar.color = {
@@ -15,3 +28,14 @@ aCar.color = {
 }
 
 console.log(  "It is a " + aCar.color.exterior + " car, " + aCar.milage + " milage, with " + aCar.color.interior.texture + " interior.")
+
+console.log(
+  "First owner: " +
+    aCar.previous_owners[0].name +
+    ' - ' +
+    aCar.previous_owners[0].address
+)
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+	console.log(aCar.previous_owners[i].name);
+}
