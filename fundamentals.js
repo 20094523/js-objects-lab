f1df90b3dad3aeb3c8ba4a73e9f60719a93f2d1c
Dominik Martynski <20094523@mail.wit.ie>
2023-08-25 13:33:18 +0100

    Initial structure.

---
+++ fundamentals.js
@@ -0,0 +1,9 @@
+const aCar = {
+    owner : "Joe Bloggs",
+    type : 'Toyota Corolla 1.9',
+    registration : '201WD1058'
+  };
+  
+  console.log(aCar.owner);
+  console.log(aCar.type);
+  console.log(aCar.registration)
